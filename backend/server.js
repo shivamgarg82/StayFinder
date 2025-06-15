4afa4d62532b0a98764db72ccaafaf9e38e1cc80
Shivam Garg <ngarg82871@gmail.com>
2025-06-15 19:28:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-// Minimal Express Server
+import connectDB from './db.js';
+import Listing from './models/Listing.js';
+
+
+
 const express = require('express');
+connectDB(); // Connect to MongoDB
 const app = express();
-app.get('/api/listings', (req, res) => {
-  res.json([{
-    id: 1,
-    title: "Beachfront Villa",
-    price: 200
-  }]);
+app.get('/api/listings', async (req, res) => {
+  const listings = await Listing.find(); // Actual DB query
+  res.json(listings);
 });
 app.listen(5000, () => console.log('API running on port 5000'));
